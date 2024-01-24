4ffda6fd5ac7e67fa94bd6a61e4455ddc65c2935
GustiG <gurgulescu@gmail.com>
2024-01-24 17:24:54 +0200

    Power Shell push with scripts

--- second/Program.cs
+++ second/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// step 1 : git --version (check if you have git installed)
+// step 2 : git clone https://github.com/your-username/your-repository.git (clone your repository locally adding .git)
+// step 3 : git add . (name of the project)
+// step 4 : git commit -m "Initial commit" 
+// step 5 : git push origin main
