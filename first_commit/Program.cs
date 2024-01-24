81fc873802ed0c80611b5345053523270a6ed370
GustiG <gurgulescu@gmail.com>
2024-01-24 17:09:49 +0200

    nice

--- first_commit/Program.cs
+++ first_commit/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// this is my first console progrgam pushed directly to git hub via Power Shell
+// this is my first console program pushed directly to git hub via Visual Studio Code
 
