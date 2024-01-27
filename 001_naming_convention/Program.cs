0bd9bb73d1195e704b63188e696a27488ac0501e
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-27 14:47:15 +0200

    001

---
+++ 001_naming_convention/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// kicking it off with the naming comvention from now going forward
+// each project will start with a number between 001 - 999
