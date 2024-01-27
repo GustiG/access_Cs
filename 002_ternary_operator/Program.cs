c9cc855652335882b296ee90c4a15b205454a6cc
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-27 15:59:37 +0200

    even / odd ?

---
+++ 002_ternary_operator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type in an integer to check if it's even or odd: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Thread.Sleep (255);
+string answer = (number % 2 == 0) ? "even" : "odd";
+Console.WriteLine($"{number} is {answer}");
+
+
