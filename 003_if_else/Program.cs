9acf5b034b4a063349095a7d9ac6762259973109
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-27 19:22:41 +0200

    password checker

---
+++ 003_if_else/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Create your username: ");
+string username = Console.ReadLine();
+
+Console.WriteLine($"Now {username}, create your password: ");
+string password = Console.ReadLine();
+
+Console.WriteLine("Repeat your password to make sure you remember it: ");
+string secondPassword = Console.ReadLine();
+
+if (password != secondPassword)
+{
+    Console.WriteLine($"You didn't enter the same password, {username}!");
+}
+else
+{
+    Console.WriteLine($"Good job, {username}. Your password is stored.");
+}
