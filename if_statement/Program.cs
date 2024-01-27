2c8ab81ea2d55baf981ebb80f5c39319f8f13e2b
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-27 16:17:11 +0200

    if and char delay

--- if_statement/Program.cs
+++ if_statement/Program.cs
@@ -1,15 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Thread.Sleep(255);
 Console.WriteLine("Please write your name: ");
  string name = Console.ReadLine();
+ string failure = "Restart and try again until I learn the loops for C#.";
+
+
 
  if (name != "")
  {
-    Console.WriteLine($"Hello, {name}. Nice to meet you");
+    Console.WriteLine($"Hello, {name}.\nNice to meet you!");
  }
  else 
  {
-    Console.WriteLine("Restart and try again until I learn the loops for C#.");
+   foreach (char c in failure)            // print every character separately
+   {
+      Thread.Sleep(111);                  // with 111 ms delay
+      Console.Write(c);                   // all on the same line
+   }
  }
 
  // Console.ReadKey();
