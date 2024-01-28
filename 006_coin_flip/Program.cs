40b1cefe035ed02ea19c05a1781d9e700ef33a64
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-28 12:30:04 +0200

    heads/tails

---
+++ 006_coin_flip/Program.cs
@@ -0,0 +1,12 @@
+﻿// challenge: do this in 3 lines of code
+
+Random coin = new Random();
+int flip = coin.Next(2);
+Console.WriteLine((flip == 0) ? "heads":"tail");
+
+// 2 lines
+
+// Random coin = new Random();
+// Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+
+// Console.WriteLine(34.40M); // prints 34.40
