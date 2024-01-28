2231a2ee345eab9302076617b2cc7ae3e7e9ef53
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-28 12:27:54 +0200

    boolean fun

---
+++ 005_vs_code_terminal/Program.cs
@@ -0,0 +1,18 @@
+﻿string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());  // true
+
+// Console.WriteLine("a" != "a");   false
+// Console.WriteLine("a" != "A");   true
+// Console.WriteLine(1 != 2);       true
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a"); false
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox")); // true
+// Console.WriteLine(pangram.Contains("cow")); // false
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));    // false
+Console.WriteLine(!pangram.Contains("cow"));    // true
