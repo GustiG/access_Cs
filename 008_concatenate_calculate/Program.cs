51c94cb1c8e47505e04a9df2a1611db47a229102
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-28 16:21:38 +0200

    ()

---
+++ 008_concatenate_calculate/Program.cs
@@ -0,0 +1,7 @@
+﻿string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets."); // concatenates
+
+string secondName = "Bob";
+int itemsSold = 7;
+Console.WriteLine(secondName + " sold " + (itemsSold + 7) + " widgets."); // calculates
