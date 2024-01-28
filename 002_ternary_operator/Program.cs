7408dfce845088eba2d5b324d42c6625dbbc17d9
GustiG <augustin_gurgulescu@yahoo.com>
2024-01-28 12:24:46 +0200

    even/odd discount

--- 002_ternary_operator/Program.cs
+++ 002_ternary_operator/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Type in an integer to check if it's even or odd: ");
+﻿Console.WriteLine("Type in an integer to check if it's even or odd: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Thread.Sleep (255);
@@ -7,3 +6,11 @@ string answer = (number % 2 == 0) ? "even" : "odd";
 Console.WriteLine($"{number} is {answer}");
 
 
+int saleAmount = 1001;
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");
+
+// or remove the temporary variable "discount"
+
+// int saleAmount = 1001;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
--- 002_ternary_operator/obj/Debug/net8.0/ternary_operator.AssemblyInfo.cs
+++ 002_ternary_operator/obj/Debug/net8.0/ternary_operator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ternary_operator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14971eae371e7bd1da5e85616fc0aca80a6e88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802378f36cb7db16c86353a4a9c31a215e9d6e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ternary_operator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ternary_operator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
