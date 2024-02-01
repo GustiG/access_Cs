009f7910bf59aa8699cf81f1f5d588a938edddb1
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-01 21:32:20 +0200

    all set

--- 011_dice_rolls/Program.cs
+++ 011_dice_rolls/Program.cs
@@ -1,14 +1,13 @@
-﻿// string greeting = "Hi there, welcome to my dice game\nThe rules are simple:\n"
-//                 + "You have 3 dices. You roll 15 or more and you win.\n"
-//                 + "Bonus 2 points for rolling dobules and +6 points for rolling triples."
-//                 + "\nGood luck!";
-// foreach (char c in greeting)
-// {
-//     Thread.Sleep(77);
-//     Console.Write(c);
-// }
-
-using System.Threading.Channels;
+﻿string greeting = "Hi there, welcome to my dice game!\nThe rules are simple:\n"
+                + "You have 3 dices. You roll 15 or more and you win.\n"
+                + "Bonus 2 points for rolling dobules and +6 points for rolling triples."
+                + "\nGood luck!\n";
+foreach (char c in greeting)
+{
+    Thread.Sleep(77);
+    Console.Write(c);
+}                                      
+ // comment everything above this line for fast paced gameplay
 
 Random dice = new Random();
 int roll1 = dice.Next(1, 7);
