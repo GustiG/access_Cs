a90f835c99118f48a4c7a8a07ee7651388951a87
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-04 17:20:31 +0200

    playing with scope

--- 015_foreach_loop/Program.cs
+++ 015_foreach_loop/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] names = ["Rowena", "Robin", "Bao"];
+// foreach (string x in names)
+// {
+//     Console.WriteLine(x);
+// }
+
+//////////////////////////////--FANCY CODE BELOW--//////////////////////////////////
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
