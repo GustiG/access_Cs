cec77f490f59b8ad4c2615168be72c1acf5c7e03
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-04 17:18:23 +0200

    it's what it's

---
+++ 016_nested_iteration/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Declare an array and initialize it to contain the following elements:
+
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+
+These values represent the fraudulent Order ID data that your application use.
+
+Create a foreach statement to iterate through each element of your array.
+
+Report the Order IDs that start with the letter "B".
+*/
+
+string[] ids = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach(string id in ids)
+{
+    if (id.StartsWith('B'))
+    {
+        Console.WriteLine(id);
+    }
+}
