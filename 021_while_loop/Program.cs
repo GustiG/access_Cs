d0feec6a10ff1dd5e2eb5f060ceb5031098e3de3
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-09 22:24:18 +0200

    counting

---
+++ 021_while_loop/Program.cs
@@ -0,0 +1,18 @@
+﻿int counter = 0;
+
+do
+{
+    counter++;
+    Console.WriteLine(counter);  // counts from 1 to 5 because the print is after the increment
+}
+while (counter < 5);
+
+// FOR LOOP below -- the for loop is optimized for counting
+
+for (   
+    int i = 0;  // setup/initialize (start) 
+    i < 5;      // conditional
+    i++)        // iteration (increment)
+{
+    Console.WriteLine(i);       // counts from 0 to 4
+}
