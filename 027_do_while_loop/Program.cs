cbe5a3f502c83eefddd32492a764e8cf8eb22886
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-11 10:32:31 +0200

    do-while

---
+++ 027_do_while_loop/Program.cs
@@ -0,0 +1,20 @@
+﻿Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;     // 8 or more will not be returned
+                                    // the continue statement transfers execution to the end of the current iteration.
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
