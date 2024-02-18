b395c39d14843460b55ded4faf05b7a40773af69
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-18 10:46:50 +0200

    will be back

---
+++ 031_continue_c_sharp/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// will continue this as soon as I gather more energy -- for now I'm focused on learning on the job
