556a1e86acbae959ed1ecf97cd547aed686aba54
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-18 20:29:02 +0200

    range[2..4] & last[^1]

--- 030_list_of_T/Program.cs
+++ 030_list_of_T/Program.cs
@@ -4,7 +4,7 @@ names.Add("David");
 names.Add("Damian");
 names.Add("Maria");             
 
-foreach (var name in names)
+foreach (var name in names[2..4])   // iterate to the 2 - 4 range (2 is included, 4 is excluded)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
@@ -15,5 +15,12 @@ for (int i = 0; i < names.Count; i++)
 {
     Console.WriteLine($"Hello {names[i].ToUpper()}!"); // .ToUpper generates a new list, the string is immutable
 }
+
+Console.WriteLine(names[0]);    // first in the list
+Console.WriteLine(names[2]);    // 3rd in the list
+Console.WriteLine(names[names.Count -1]);    // last one OR
+Console.WriteLine(names[^1]);   // count 1 from the back of the list
+
+
 
 
--- 030_list_of_T/obj/Debug/net8.0/030_list_of_T.AssemblyInfo.cs
+++ 030_list_of_T/obj/Debug/net8.0/030_list_of_T.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("030_list_of_T")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d980ef8e37161b16a0354ab89ce0c2c0e4673aa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b395c39d14843460b55ded4faf05b7a40773af69")]
 [assembly: System.Reflection.AssemblyProductAttribute("030_list_of_T")]
 [assembly: System.Reflection.AssemblyTitleAttribute("030_list_of_T")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
