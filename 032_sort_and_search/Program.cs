b78d70d8a8c0556cd704cb56a502044db7578e51
GustiG <augustin_gurgulescu@yahoo.com>
2024-02-19 21:41:10 +0200

    all good

---
+++ 032_sort_and_search/Program.cs
@@ -0,0 +1,22 @@
+﻿var names = new List<string> { "Scott", "Ana", "Felipe" };
+
+names.Sort();
+
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
+var numbers = new List<int> { 45, 56, 99, 48, 67, 78 };
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)}");
+numbers.Sort();
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)}");
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+// name.Sort() 
+// name.IndexOf();
