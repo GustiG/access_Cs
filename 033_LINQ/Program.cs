832dd9b278772f5b570eaa9434a6b0d834ae0a8c
GustiG <augustin_gurgulescu@yahoo.com>
2024-03-10 20:56:21 +0200

    LINQ

---
+++ 033_LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿// Specify the data source
+List<int> scores = [97, 92, 81, 60]; // same as int[] scores = [1, 2] // OR var scores = new List<int> [1, 2]
+
+// find all the scores that are over 80 (using declarative programming) -- without LINQ
+
+for (int i = 0; i < scores.Count; i++)
+{
+    if (scores[i] > 80)
+    {
+        Console.WriteLine($"Found it {scores[i]}");
+    }
+}
+
+// Define the query expression -- using LINQ
+IEnumerable<int> scoreQuery =
+    from score in scores
+    where score > 80
+    select score;
+
+// Execute the query
+foreach (int i in scoreQuery)
+{
+    Console.Write(i + " ");     // i + whitespace
+}
